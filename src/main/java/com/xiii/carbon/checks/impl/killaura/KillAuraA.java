be3d5132503433649d95fcf66be628d0cc1ca093
XIII___ <cyjallet@gmail.com>
2023-07-06 20:09:04 +0200

    Code cleanup

--- src/main/java/com/xiii/carbon/checks/impl/killaura/KillAuraA.java
+++ src/main/java/com/xiii/carbon/checks/impl/killaura/KillAuraA.java
@@ -1,38 +1,34 @@
 package com.xiii.carbon.checks.impl.killaura;
 
-import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.xiii.carbon.checks.annotation.Experimental;
 import com.xiii.carbon.checks.enums.CheckType;
 import com.xiii.carbon.checks.types.Check;
 import com.xiii.carbon.managers.profile.Profile;
 import com.xiii.carbon.processors.packet.ClientPlayPacket;
 import com.xiii.carbon.processors.packet.ServerPlayPacket;
-import org.bukkit.entity.Entity;
 
 @Experimental
 public class KillAuraA extends Check {
-    private int hitEntities;
+    public KillAuraA(final Profile profile) {
+        super(profile, CheckType.KILLAURA, "A", "Checks if player hits more entities than possible.");
+    }
+
     private int lastHitEntity;
 
-    public KillAuraA(final Profile profile) {
-        super(profile, CheckType.KILLAURA, "A", "Checks if player's hit more Entities than possible.");
-    }
-
     @Override
-    public void handle(ClientPlayPacket clientPlayPacket) {
+    public void handle(final ClientPlayPacket clientPlayPacket) {
+
         if (clientPlayPacket.isAttack()) {
+
             final int currentEntity = clientPlayPacket.getInteractEntityWrapper().getEntityId();
-            if (currentEntity != lastHitEntity) {
-                hitEntities++;
-                if (hitEntities > 1) fail("c=" + hitEntities);
-            }
+
+            if (currentEntity != lastHitEntity && increaseBuffer() > 1) fail("c=" + getBuffer());
+
             lastHitEntity = currentEntity;
-        } else if (clientPlayPacket.isFlying()) {
-            hitEntities = 0;
-        }
+        } else if (clientPlayPacket.isFlying()) resetBuffer();
 
     }
 
     @Override
-    public void handle(ServerPlayPacket serverPlayPacket) {}
+    public void handle(final ServerPlayPacket serverPlayPacket) {}
 }
