10eb01f301984a1ea90fc1c14e8663bd45beb32a
XIII___ <cyjallet@gmail.com>
2023-07-06 20:13:42 +0200

    Code cleanup

--- src/main/java/com/xiii/carbon/checks/impl/killaura/KillAuraB.java
+++ src/main/java/com/xiii/carbon/checks/impl/killaura/KillAuraB.java
@@ -6,32 +6,36 @@ import com.xiii.carbon.checks.types.Check;
 import com.xiii.carbon.managers.profile.Profile;
 import com.xiii.carbon.processors.packet.ClientPlayPacket;
 import com.xiii.carbon.processors.packet.ServerPlayPacket;
-import com.xiii.carbon.utils.MathUtils;
 
 @Experimental
 public class KillAuraB extends Check {
-
-    private long UseEntityPackets;
-
-    public KillAuraB(Profile profile) {
+    public KillAuraB(final Profile profile) {
         super(profile, CheckType.KILLAURA, "B", "Checks the player's packet timing.");
     }
 
+    private long UseEntityPackets;
+
     @Override
-    public void handle(ClientPlayPacket clientPlayPacket) {
+    public void handle(final ClientPlayPacket clientPlayPacket) {
+
         if (clientPlayPacket.isAttack()) {
+
             final long lastFlying = System.currentTimeMillis() - UseEntityPackets;
+
             if (lastFlying < 30L) {
-                if (increaseBufferBy(1) > 1) fail("t=" + lastFlying);
+
+                if (increaseBuffer() > 1) fail("t=" + lastFlying);
             } else decreaseBufferBy(1);
+
         } else if (clientPlayPacket.isFlying()) {
+
             UseEntityPackets = System.currentTimeMillis();
             decreaseBufferBy(0.1);
         }
     }
 
     @Override
-    public void handle(ServerPlayPacket serverPlayPacket) {
+    public void handle(final ServerPlayPacket serverPlayPacket) {
 
     }
 }
