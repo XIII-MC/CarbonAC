eaf5b90b2d4d21693e60a134234fbc2a93c0477b
XIII___ <cyjallet@gmail.com>
2023-07-08 16:05:46 +0200

    Made it a real check

--- src/main/java/com/xiii/carbon/checks/impl/velocity/VelocityA.java
+++ src/main/java/com/xiii/carbon/checks/impl/velocity/VelocityA.java
@@ -1,8 +1,6 @@
 package com.xiii.carbon.checks.impl.velocity;
 
-import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.util.Vector3d;
-import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerEntityVelocity;
 import com.xiii.carbon.checks.annotation.Experimental;
 import com.xiii.carbon.checks.enums.CheckType;
 import com.xiii.carbon.checks.types.Check;
@@ -11,45 +9,45 @@ import com.xiii.carbon.playerdata.data.impl.CombatData;
 import com.xiii.carbon.playerdata.data.impl.MovementData;
 import com.xiii.carbon.processors.packet.ClientPlayPacket;
 import com.xiii.carbon.processors.packet.ServerPlayPacket;
-import com.xiii.carbon.utils.MathUtils;
 import com.xiii.carbon.utils.MoveUtils;
 
 @Experimental
 public class VelocityA extends Check {
-
-    private Vector3d velocityVector;
-    private int ticks = 0;
-    private boolean hit = false;
-
     public VelocityA(final Profile profile) {
         super(profile, CheckType.VELOCITY, "A", "Check's difference between player's and server's velocity.");
     }
 
+    private int ticks = 0;
+
     @Override
-    public void handle(ClientPlayPacket clientPlayPacket) {
+    public void handle(final ClientPlayPacket clientPlayPacket) {
+
         if (clientPlayPacket.isMovement()) {
+
             final MovementData movementData = profile.getMovementData();
-            //if (!movementData.isBlockAbove()) {
-            final boolean jumped = !movementData.isOnGround() && movementData.isLastOnGround();
-            if (hit) {
-                if(jumped || (ticks++ > (MoveUtils.getMaxVelocityTicks(0, velocityVector.getY()))) ) {
-                    final double percentage = movementData.getDeltaY() / velocityVector.getY() * 100;
-                    debug("percentage=" + percentage);
+            final CombatData combatData = profile.getCombatData();
+
+            final boolean jumped = !movementData.isOnGround() && movementData.isLastOnGround() && movementData.getDeltaY() == MoveUtils.JUMP_MOTION;
+
+            final Vector3d entityVelocity = combatData.getEntityVelocity();
+
+            if (entityVelocity != null && entityVelocity.getY() != 0) {
+
+                if(jumped || (ticks++ > (MoveUtils.getMaxVelocityTicks(0, entityVelocity.getY()))) ) {
+
+                    final double percentage = movementData.getDeltaY() / entityVelocity.getY() * 100;
+
+                    if ((percentage >= 0 && percentage < 100) && increaseBuffer() > 2) {
+                        fail("percentage=" + percentage);
+                        debug("%: " + percentage);
+                    } else decreaseBufferBy(1);
+
                     ticks = 0;
-                    hit = false;
                 }
             }
         }
     }
 
     @Override
-    public void handle(ServerPlayPacket serverPlayPacket) {
-        if (serverPlayPacket.is(PacketType.Play.Server.ENTITY_VELOCITY)) {
-            WrapperPlayServerEntityVelocity veloWrapper = serverPlayPacket.getEntityVelocityWrapper();
-            if (veloWrapper.getEntityId() == profile.getPlayer().getEntityId()) {
-                velocityVector = veloWrapper.getVelocity();
-                hit = true;
-            }
-        }
-    }
+    public void handle(final ServerPlayPacket serverPlayPacket) {}
 }
