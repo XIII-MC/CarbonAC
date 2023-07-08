2235702b0500707d86d0f12d0f7a4a465012523f
XIII___ <cyjallet@gmail.com>
2023-07-08 16:05:17 +0200

    Trying to damage handle fly

--- src/main/java/com/xiii/carbon/checks/impl/fly/FlyA.java
+++ src/main/java/com/xiii/carbon/checks/impl/fly/FlyA.java
@@ -1,10 +1,10 @@
 package com.xiii.carbon.checks.impl.fly;
 
-import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.xiii.carbon.checks.annotation.Experimental;
 import com.xiii.carbon.checks.enums.CheckType;
 import com.xiii.carbon.checks.types.Check;
 import com.xiii.carbon.managers.profile.Profile;
+import com.xiii.carbon.playerdata.data.impl.CombatData;
 import com.xiii.carbon.playerdata.data.impl.MovementData;
 import com.xiii.carbon.processors.PredictionEngine;
 import com.xiii.carbon.processors.packet.ClientPlayPacket;
@@ -26,6 +26,7 @@ public class FlyA extends Check {
         if (!clientPlayPacket.isMovement()) return;
 
         final MovementData movementData = profile.getMovementData();
+        final CombatData combatData = profile.getCombatData();
 
         final boolean exempt = profile.isExempt().isFly() || profile.isExempt().isWater(150L) || profile.isExempt().isLava(150L) || profile.isExempt().isTrapdoor_door() || profile.isExempt().isCobweb(50L) || profile.isExempt().isCake() || profile.getVehicleData().isRiding(150L) || (profile.isExempt().isJoined(5000L) && movementData.isServerGround()) || profile.isExempt().isClimable(50L) || profile.isExempt().tookDamage(50L);
 
@@ -43,12 +44,12 @@ public class FlyA extends Check {
 
             final boolean jumped = (!movementData.isOnGround() && movementData.isLastOnGround() && deltaY == MoveUtils.JUMP_MOTION) || (!movementData.isOnGround() && movementData.getLastNearWallTicks() > 0 && (deltaY == 0.40444491418477835 || deltaY == 0.33319999363422337));
 
+            if (combatData.getEntityVelocity() != null && combatData.getEntityVelocity().getY() != 0) debug("FVY=" + ((deltaY - combatData.getEntityVelocity().getY() - 0.0784) - predictionOutput));
+
             if (!nearGroundExempt && !exempt && prediction > predictionLimit && !jumped) {
                 fail("pred=" + predictionOutput + " my=" + deltaY);
-               // debug("pred=" + predictionOutput + " my=" + deltaY + " ngt=" + movementData.getClientGroundTicks() + " offset=" + (prediction - 1.9262653090336062E-14));
             }
-
-        } else decreaseBufferBy(1);
+        }
     }
 
     @Override
