9c4cdabfa52ae9c7be3a51fe07c51f1f18fc314b
XIII___ <cyjallet@gmail.com>
2023-07-09 20:28:52 +0200

    "Handling" damage

--- src/main/java/com/xiii/carbon/checks/impl/fastclimb/FastClimbA.java
+++ src/main/java/com/xiii/carbon/checks/impl/fastclimb/FastClimbA.java
@@ -1,10 +1,12 @@
 package com.xiii.carbon.checks.impl.fastclimb;
 
+import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.xiii.carbon.checks.annotation.Experimental;
 import com.xiii.carbon.checks.enums.CheckType;
 import com.xiii.carbon.checks.types.Check;
 import com.xiii.carbon.managers.profile.Profile;
 import com.xiii.carbon.playerdata.data.impl.MovementData;
+import com.xiii.carbon.playerdata.data.impl.TeleportData;
 import com.xiii.carbon.processors.packet.ClientPlayPacket;
 import com.xiii.carbon.processors.packet.ServerPlayPacket;
 import com.xiii.carbon.utils.MoveUtils;
@@ -14,6 +16,9 @@ public class FastClimbA extends Check {
     public FastClimbA(final Profile profile) {
         super(profile, CheckType.FASTCLIMB, "A", "Checks if the player is moving too fast/slow on a climable.");
     }
+
+    private double addVelocity = 0;
+    private long lastVelocity = System.currentTimeMillis();
 
     @Override
     public void handle(final ClientPlayPacket clientPlayPacket) {
@@ -31,12 +36,12 @@ public class FastClimbA extends Check {
             if (!exempt && profile.isExempt().isClimable(50L)) {
 
                 if (movementData.getAirTicks() > 4) {
-                    if (deltaY > 0.1177) {
+                    if (deltaY > 0.1177 + addVelocity) {
                         fail("deltaY: " + deltaY);
                         debug("FastClimb A - dY=" + deltaY + " aT=" + movementData.getAirTicks() + " lB=" + lowBlock);
                     }
                 } else {
-                    if (deltaY > (MoveUtils.JUMP_MOTION + (lowBlock ? 0.5 : 0))) {
+                    if (deltaY > (MoveUtils.JUMP_MOTION + (lowBlock ? 0.5 : 0) + addVelocity)) {
                         fail("Exceeded maximum motion: " + deltaY);
                         debug("FastClimb A MAX - dY=" + deltaY + " aT=" + movementData.getAirTicks() + " lB=" + lowBlock + " bbT=" + movementData.getBlockBelowTicks());
                     }
@@ -46,5 +51,11 @@ public class FastClimbA extends Check {
     }
 
     @Override
-    public void handle(final ServerPlayPacket serverPlayPacket) {}
+    public void handle(final ServerPlayPacket serverPlayPacket) {
+        if (serverPlayPacket.is(PacketType.Play.Server.ENTITY_VELOCITY)) {
+            addVelocity = Math.abs(serverPlayPacket.getEntityVelocityWrapper().getVelocity().getY());
+            lastVelocity = System.currentTimeMillis();
+            debug( "VELOCITY=" + serverPlayPacket.getEntityVelocityWrapper().getVelocity().getY());
+        } else if (System.currentTimeMillis() - lastVelocity > 650) addVelocity = 0;
+    }
 }
