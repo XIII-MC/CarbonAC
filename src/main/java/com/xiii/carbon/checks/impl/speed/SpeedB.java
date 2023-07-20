ece2a3b1f95ba905f7d55a08be8add7d0abfe4be
XIII___ <cyjallet@gmail.com>
2023-07-20 11:52:46 +0200

    Improved buffer system

--- src/main/java/com/xiii/carbon/checks/impl/speed/SpeedB.java
+++ src/main/java/com/xiii/carbon/checks/impl/speed/SpeedB.java
@@ -13,27 +13,27 @@ import com.xiii.carbon.utils.MoveUtils;
 @Experimental
 public class SpeedB extends Check {
     public SpeedB(final Profile profile) {
-        super(profile, CheckType.SPEED, "B", "Player is not following Minecraft's vanilla friction");
+        super(profile, CheckType.SPEED, "B", "Player does not obey Minecraft's friction laws");
     }
 
     @Override
     public void handle(final ClientPlayPacket clientPlayPacket) {
 
-        if (clientPlayPacket.isMovement()) {
+        if (!clientPlayPacket.isMovement()) return;
 
-            final MovementData movementData = profile.getMovementData();
-            final ActionData actionData = profile.getActionData();
+        final MovementData movementData = profile.getMovementData();
+        final ActionData actionData = profile.getActionData();
 
-            final boolean exempt = profile.isExempt().isFly() || (profile.isExempt().isJoined(5000L) && movementData.isServerGround()) || profile.isExempt().getTeleportTicks() <= 2;
+        final boolean exempt = profile.isExempt().isFly() || (profile.isExempt().isJoined(2000L) && movementData.isServerGround()) || profile.isExempt().getTeleportTicks() <= 2 || profile.isExempt().isClimable(50L);
 
-            if (!exempt && movementData.getAirTicks() > 2) {
+        if (!exempt && movementData.getAirTicks() > 2) {
 
-                final double friction = movementData.getDeltaXZ() - (movementData.getLastDeltaXZ() * MoveUtils.FRICTION + (actionData.isSprinting() ? 0.026 : 0.02));
+            final double friction = movementData.getDeltaXZ() - (movementData.getLastDeltaXZ() * MoveUtils.FRICTION + (actionData.isSprinting() ? 0.026 : 0.02));
 
-                if (friction > 0) {
-                    if (increaseBuffer() > 1) fail("friction=" + friction);
-                } else decreaseBufferBy(1);
-            }
+            if (friction > 0) {
+                if (increaseBuffer() > (movementData.getLastNearWallTicks() <= 4 ? 1 : 0))
+                    fail("friction: §c" + friction + System.lineSeparator() + "§rlNGT: §c" + movementData.getLastNearWallTicks() + System.lineSeparator() + "§rbuffer: §c" + getBuffer());
+            } else decreaseBufferBy(0.10);
         }
     }
 
